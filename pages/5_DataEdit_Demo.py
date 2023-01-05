b64fcfe96060286712bcfcffb7d6101667028ff2
Efendi Lim <54058259+efendilim@users.noreply.github.com>
2023-01-05 22:04:09 +0800

    Update 5_DataEdit_Demo.py

--- pages/5_DataEdit_Demo.py
+++ pages/5_DataEdit_Demo.py
@@ -3,7 +3,6 @@ import json
 import pandas as pd
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from snowflake.snowpark import Session, Row
 import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 
